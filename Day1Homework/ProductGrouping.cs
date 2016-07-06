4980da2ee34d856c085b4c5388ea4b39049b0336
陳建忠(網技一課) <chienchung.chen@housefun.com.tw>
2016-07-07 06:52:10 +0800

    補上測試案例

--- Day1Homework/ProductGrouping.cs
+++ Day1Homework/ProductGrouping.cs
@@ -8,34 +8,14 @@ namespace Day1Homework
 {
     public class ProductGrouping
     {
-        public List<Product> GetProductList()
-        {
-            var products = new List<Product>();
-            products.Add(new Product { Id = 1, Cost = 1, Revenue = 11, SellPrice = 21 });
-            products.Add(new Product { Id = 2, Cost = 2, Revenue = 12, SellPrice = 22 });
-            products.Add(new Product { Id = 3, Cost = 3, Revenue = 13, SellPrice = 23 });
-            products.Add(new Product { Id = 4, Cost = 4, Revenue = 14, SellPrice = 24 });
-            products.Add(new Product { Id = 5, Cost = 5, Revenue = 15, SellPrice = 25 });
-            products.Add(new Product { Id = 6, Cost = 6, Revenue = 16, SellPrice = 26 });
-            products.Add(new Product { Id = 7, Cost = 7, Revenue = 17, SellPrice = 27 });
-            products.Add(new Product { Id = 8, Cost = 8, Revenue = 18, SellPrice = 28 });
-            products.Add(new Product { Id = 9, Cost = 9, Revenue = 19, SellPrice = 29 });
-            products.Add(new Product { Id = 10, Cost = 10, Revenue = 20, SellPrice = 30 });
-            products.Add(new Product { Id = 11, Cost = 11, Revenue = 21, SellPrice = 31 });
-
-            return products;
-        }
-
         /// <summary>
         /// 取得產品幾個一組的加總
         /// </summary>
         /// <param name="count">個數(幾個一組)</param>
         /// <param name="colName">欄位名稱</param>
         /// <returns></returns>
-        public decimal[] GetProductSubTotal(int count, string colName)
+        public decimal[] GetProductSubTotal<T>(int count, string colName, List<T> productList)
         {
-            List<Product> productList = GetProductList();
-
             int groupCount = (int)Math.Ceiling((double)productList.Count() / count);
             decimal[] result = new decimal[groupCount];
 
--- Day1HomeworkTests/ProductGroupingTests.cs
+++ Day1HomeworkTests/ProductGroupingTests.cs
@@ -12,6 +12,28 @@ namespace Day1Homework.Tests
     [TestClass()]
     public class ProductGroupingTests
     {
+        private class Product
+        {
+            public int Id { get; set; }
+            public int Cost { get; set; }
+            public int Revenue { get; set; }
+            public int SellPrice { get; set; }
+        }
+
+        private List<Product> testData = new List<Product>() {
+                new Product {Id=1,Cost=1,Revenue=11,SellPrice=21 },
+                new Product {Id=2,Cost=2,Revenue=12,SellPrice=22 },
+                new Product {Id=3,Cost=3,Revenue=13,SellPrice=23 },
+                new Product {Id=4,Cost=4,Revenue=14,SellPrice=24 },
+                new Product {Id=5,Cost=5,Revenue=15,SellPrice=25 },
+                new Product {Id=6,Cost=6,Revenue=16,SellPrice=26 },
+                new Product {Id=7,Cost=7,Revenue=17,SellPrice=27 },
+                new Product {Id=8,Cost=8,Revenue=18,SellPrice=28 },
+                new Product {Id=9,Cost=9,Revenue=19,SellPrice=29 },
+                new Product {Id=10,Cost=10,Revenue=20,SellPrice=30 },
+                new Product {Id=11,Cost=11,Revenue=21,SellPrice=31 }
+        };
+
         [TestMethod()]
         public void GetProductSubTotalTest_3筆一組_取Cost總和_結果應為6_15_24_21()
         {
@@ -21,7 +43,7 @@ namespace Day1Homework.Tests
             var sut = new ProductGrouping();
 
             // atc
-            var actual = sut.GetProductSubTotal(size, colName);
+            var actual = sut.GetProductSubTotal(size, colName, testData);
 
             // assert
             var expected = new decimal[] { 6, 15, 24, 21 };
@@ -38,7 +60,7 @@ namespace Day1Homework.Tests
             var sut = new ProductGrouping();
 
             // atc
-            var actual = sut.GetProductSubTotal(size, colName);
+            var actual = sut.GetProductSubTotal(size, colName, testData);
 
             // assert
             var expected = new decimal[] { 50, 66, 60 };
